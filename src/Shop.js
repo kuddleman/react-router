f8cc687aa33ea1d362632017b08fe704e568288a
Donny <EDL1155@gmail.com>
2020-03-01 19:41:29 -0800

    initial commit

---
+++ src/Shop.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+
+
+
+function Shop() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+ 
+  const fetchItems = async () => {
+    const data = await fetch('https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get'
+    );
+
+    const items = await data.json();
+    console.log(items)
+
+  }
+
+  return (
+    <div>
+      <h1>Hello from Shop</h1>
+      
+    </div>
+  );
+}
+
+export default Shop;
